d9ccb3c5931c0e052c935036eb4028d1538901ec
frynana <anna.helloiza.santos@escola.pr.gov.br>
2025-09-10 12:20:17 -0300

    Base da lista de anime

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -9,7 +9,7 @@ import { ThemedView } from '@/components/ThemedView';
 export default function HomeScreen() {
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerBackgroundColor={{ light: '#2f2f2f', dark: '#2f2f2f' }}
       headerImage={
         <Image
           source={require('@/assets/images/partial-react-logo.png')}
@@ -17,24 +17,25 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
+        <ThemedText type="title">Animes</ThemedText>
       </ThemedView>
+      <div>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
+        <ThemedText type="subtitle">Kaiju n° 8</ThemedText>
         <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
+        ★ <ThemedText type="defaultSemiBold">4.9</ThemedText> Kaiju No. 8 acompanha Kafka Nagai, um segurança que, ao ganhar a habilidade de se transformar em um kaiju de nível 8, tenta equilibrar vida, trabalho e segredos, enfrentando monstros gigantes, conspirações e escolhas que definem quem é humano, herói ou monstro.
+          ficção e ação{' '}
           <ThemedText type="defaultSemiBold">
             {Platform.select({
               ios: 'cmd + d',
               android: 'cmd + m',
-              web: 'F12',
+              web: '',
             })}
           </ThemedText>{' '}
-          to open developer tools.
+          #1111 Ranking
         </ThemedText>
       </ThemedView>
+      </div>
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 2: Explore</ThemedText>
         <ThemedText>
